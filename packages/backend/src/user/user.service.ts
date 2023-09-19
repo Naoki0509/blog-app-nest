141eb70bd44b8228e62e9ba392e05fabb0477d79
“Naoki0509” <hmcn3176naoki8086@gmail.com>
2023-09-19 13:32:32 +0000

    add user Service

--- packages/backend/src/user/user.service.ts
+++ packages/backend/src/user/user.service.ts
@@ -13,4 +13,44 @@ export class UserService {
       where: userWhereUniqueInput,
     });
   }
+
+  async users(prams: {
+    skip?: number;
+    take: number;
+    cursor?: Prisma.UserWhereUniqueInput;
+    where?: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByWithRelationInput;
+  }): Promise<User[]> {
+    const { skip, take, cursor, where, orderBy } = prams;
+    return this.prisma.user.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
+    return this.prisma.user.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({
+      where,
+    });
+  }
 }
