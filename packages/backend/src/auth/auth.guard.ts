cfc6476449806809a6d76cf23c7fff73547bdd82
“Naoki0509” <hmcn3176naoki8086@gmail.com>
2023-10-18 16:08:53 +0000

    fea: back-authgard

---
+++ packages/backend/src/auth/auth.guard.ts
@@ -0,0 +1,31 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { decode } from 'next-auth/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authorization = request.headers.authorization;
+
+    if (!authorization) return false;
+
+    const token = authorization.split(' ')[1];
+
+    if (!token) return false;
+
+    const secret = process.env.NEXTAUTH_SECRET ?? '';
+
+    if (!secret) return false;
+
+    try {
+      const decoded = await decode({ token, secret });
+      if (!decoded) return false;
+
+      request.user = decoded;
+      return true;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  }
+}
